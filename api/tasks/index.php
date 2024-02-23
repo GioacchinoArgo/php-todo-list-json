9e5d50fd767b5a4085b9dd1e519f1f132a686b71
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-02-23 01:16:54 +0100

    added content on js html and index.php

---
+++ api/tasks/index.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json');
+
+$source_path = __DIR__ .'/../../database/tasks.json';
+
+$json_data = file_get_contents($source_path);
+
+$tasks = json_decode($json_data, true);
+
+$new_task_text = $_POST['task'] ?? '';
+
+if ($new_task_text) {
+    $new_task = [
+        'id' => count($tasks) + 1,
+        'text' => $new_task_text,
+        'done' => false
+    ];
+
+    $tasks[] = $new_task;
+
+    file_put_contents($source_path, json_encode($tasks));
+    echo json_encode($new_task);
+} else {
+    echo $json_data;
+}
+?>
--- my_app/js/script.js
+++ my_app/js/script.js
@@ -1,7 +1,27 @@
 const { createApp } = Vue;
+const endpoint = 'http://localhost/boolean/php-todo-list-json/api/tasks/';
 
 const app = createApp({
+    data: () => ({
+        tasks: [],
+        newTask: ''
+    }),
+    methods: {
+        addTask() {
+            const data = { task: this.newTask };
+            const config = { headers: { 'Content-Type': 'multipart/form-data' } }
 
-})
+            axios.post(endpoint, data, config).then(res => {
+                this.tasks.push(res.data);
+                this.newTask = '';
+            })
+        }
+    },
+    created() {
+        axios.get(endpoint).then(res => {
+            this.tasks = res.data;
+        });
+    }
+});
 
-app.mount('#app')
+app.mount('#app');
